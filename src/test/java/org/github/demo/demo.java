2485ac4467aa4768fe9d7aeaab4016c50dabc099
Satya <Satya@LAPTOP-1IMAFSP4.Dlink>
2022-03-26 17:44:30 +0530

    Hello All,
    it is my first commit.

---
+++ src/test/java/org/github/demo/demo.java
@@ -0,0 +1,13 @@
+package org.github.demo;
+
+public class demo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=80;
+		System.out.println(a+b);
+	}
+
+}
